466bcc2a24d95b2d22c0602e1817bb0077f272e1
Fabien <locwallers@gmail.com>
2024-04-02 11:36:44 +0200

    mise en place react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
+import Banner from './Components/Banner/Banner.js'
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        Page d'accueil
+        <nav>
+          <a href="/">Accueil</a>
+          <a href="/aPropos">A propos</a>
+        </nav>
+      </div>
+    ),
+  },
+  {
+    path: '/aPropos',
+    element: (
+      <div>
+        A Propos
+        <nav>
+          <a href="/">Accueil</a>
+          <a href="/aPropos">A propos</a>
+        </nav>
+      </div>
+    ),
+  },
+  {
+    path: '/logement',
+    element: (
+      <div>
+        Fiche Logement
+        <nav>
+          <a href="/">Accueil</a>
+          <a href="/aPropos">A propos</a>
+        </nav>
+      </div>
+    ),
+  },
+  {
+    path: '/404',
+    element: (
+      <div>
+        Erreur 404
+        <nav>
+          <a href="/">Accueil</a>
+          <a href="/aPropos">A propos</a>
+        </nav>
+      </div>
+    ),
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kaza Immobilier
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+
+
+  //  <div className="App">
+ ////     <header className="App-header">
+//      <Banner>
+//      <img src={logo} alt='La maison jungle' className='lmj-logo' />
+	//			<h1 className='lmj-title'>La maison jungle</h1>
+	//		</Banner>
+       
+  ///      <p>
+  ///        Kaza Immobilier
+  ///      </p>
+     ///     <a
+    ///        className="App-link"
+     /// ///        href="https://reactjs.org"
+  ///          target="_blank"
+   ///        rel="noopener noreferrer"
+    ///      >
+   ///         Learn React
+        ///  </a>
+        ///</header>
+      ///</div>
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,12 @@
+function Banner() {
+
+	return (
+		<div>
+	<div className='lmj-banner'></div>
+	<h1> Bonjour </h1>
+		
+		</div>
+	)
+}
+
+export default Banner
