d40c9a00c135e96b5fafb7cc97c78f0a8b662753
Fabien <locwallers@gmail.com>
2024-04-03 01:30:13 +0200

    gestion des erreurs

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -1,20 +1,28 @@
-import Imageaccueil from '../Assets/Imageaccueil.png';
+
 import './CardList.scss';
+import data from '../../Datas/Logements.json';
 import { NavLink } from 'react-router-dom';
 
 
-function Card() {
+function CardList() {
 	
 
     return (
-        <section >
-            <div  className='lmj-CardList'>
-            
+      <section>
+      <div className='lmj-CardList' class="grid-container">
+        {data.map((Logements) => (
+          <div key={Logements.id} className="grid-item">
+            <div style={{ position: "relative" }}>
+              <img src={Logements.cover} alt={Logements.title} />
+              <div className="image-title" style={{ position: "absolute", bottom: "1%", left: "1%", backgroundColor: "rgba(0, 0, 0, 0.7)", color: "white", padding: "5px", fontSize: "12px" }}>{Logements.title}</div>
+            </div>
           </div>
-        </section>
+        ))}
+      </div>
+    </section>
       );
     };
 
 
 
-export default Card
+export default CardList
--- src/routerConfig.js
+++ src/routerConfig.js
@@ -42,9 +42,35 @@ function SectionAccueil() {
 
 function PageErreur() {
   return (
-    <div style={{ maxwidth: '1440px', height: '559px'}}>
-      Erreur 404
+    <div className="main2" style={{ maxwidth: '1440px', height: '576px'}}>
+    
+    <div style={{ 
+    fontFamily : "Montserrat",
+    fontSize: '200px',
+    fontWeight :"700",
+    color:"#FF6060",
+    lineHeight: '410.69px',
+    textAlign: 'center',
+    Width: '597px',
+    Height: '263px'
+}}>
+     404
+</div>
+  <div style={{ 
+    width: '872px', 
+    height: '51px',
+     color:"#FF6060", 
+     fontSize: '28px',
+     margin:"60px",
+     textAlign:'center'}}>
+
+
+  Oups! La page que vous demandez n'existe pas.
     </div>
+    <div style={{ maxwidth: '284px', height: '96px', paddingTop :"30px"}}>
+    <NavLink className='lmj-title' to='/'> Retourner sur la page d’accueil </NavLink>  
+    </div>
+       </div>
   );
 }
 
